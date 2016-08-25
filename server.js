a6dd82e57cb90eaf80042caffe7e5f583485385d
talmasc <eric.l.hillenbrand@gmail.com>
2016-08-25 12:09:12 -0700

    finished section 4

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var auth = require('./controllers/auth');
 var message = require('./controllers/message');
+var checkAuthenticated = require('./services/checkAuthenticated');
+var cors = require('./services/cors');
 
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
-app.get("/api/message", message.get);
+app.use(cors);
 
-app.post("/api/message", message.post);
+app.get('/api/message', message.get);
+
+app.post('/api/message', checkAuthenticated, message.post);
 
 app.post('/auth/register', auth.register);
 
-mongoose.connect("mongodb://localhost:27017/test", function(err, db) {
-  if(!err) {
-    console.log("we are connected to mongo");
-  }
-});
 
-var server = app.listen(5000, function() {
-  console.log("listening on port ", server.address().port);
-});
+
+mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
+    if (!err) {
+        console.log("we are connected to mongo");
+    }
+})
+
+var server = app.listen(5000, function () {
+    console.log('listening on port ', server.address().port)
+})
